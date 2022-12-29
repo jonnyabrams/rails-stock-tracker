eb15932b887de5447c14de71c8843cb686d81323
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-29 14:56:40 +0000

    change login/register route names & shared links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "welcome#index"
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'register', to: 'devise/registrations#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
 end
