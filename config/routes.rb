22323a8cba4f88f979e077dce02ff68d3e4525ac
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-29 14:58:52 +0000

    rails g controller users my_portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/my_portfolio'
   devise_for :users
   root "welcome#index"
+  get "my_portfolio", to: "users#my_portfolio"
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
