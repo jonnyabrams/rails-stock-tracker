3607abf2c05f1b294b21086cca359c2e32538df3
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-29 15:44:29 +0000

    error alert for invalid symbol

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,7 +3,13 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      render 'users/my_portfolio'
+      # if @stock is not nil (as stipulated in Stock model's begin/rescue block)
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "Please enter a valid symbol"
+        redirect_to my_portfolio_path
+      end
     else
       flash[:alert] = "Please enter a symbol to search"
       redirect_to my_portfolio_path
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,11 @@ class Stock < ApplicationRecord
       secret_token: 'secret_token',
       endpoint: 'https://cloud.iexapis.com/v1'
     )
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    # like try/catch block
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    rescue => exception
+      return nil
+    end
   end
 end
