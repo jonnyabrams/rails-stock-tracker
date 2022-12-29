4c280aceb8f383f998584444c0b6cce3bc13e434
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-29 14:46:51 +0000

    Stock model with publishable key stored in credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publishable_key],
+      secret_token: 'secret_token',
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+    client.quote(ticker_symbol).latest_price
+  end
 end
