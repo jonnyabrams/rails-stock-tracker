b917b82c71bfc12aae0bd085610fd9bd309a11a5
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-29 15:21:03 +0000

    search_stock page after search submit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "welcome#index"
   get "my_portfolio", to: "users#my_portfolio"
+  get 'search_stock', to: 'stocks#search'
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
