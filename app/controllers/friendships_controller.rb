54ca470cd12a1ab7676a523cc4b941dc946175a4
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-01 17:59:37 +0000

    can follow friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
-
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Following #{friend.full_name}"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
