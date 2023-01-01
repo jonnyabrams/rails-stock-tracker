49082ee8b99f68a69378d96e8304e2ef5d7b2ef3
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-01 10:23:50 +0000

    user can't track stock they already track

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
   
   def under_stock_limit?
     stocks.count < 10
   end
 
   def can_track_stock?(ticker_symbol)
-    under_stock_limit?
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
 end
