17055ef9961fe38efc118ab96046576a4dcc9c51
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-01 16:27:33 +0000

    my_friends page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "my_portfolio", to: "users#my_portfolio"
   get 'search_stock', to: 'stocks#search'
+  get "my_friends", to: "users#my_friends"
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
