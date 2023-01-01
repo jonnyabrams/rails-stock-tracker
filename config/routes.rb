4261d3a22c166c3796c390b59aaf468c15c5fe28
jonnyabrams <jonnyabrams@hotmail.com>
2023-01-01 17:51:53 +0000

    can unfollow friend

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get "my_friends", to: "users#my_friends"
   get "search_friend", to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
 
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
